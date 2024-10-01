8a8c037c6d21c91f741fb9fc38fa2e7e01d6118f
Ankit Desai <ankitdesai@microsoft.com>
2024-10-01 13:25:27 -0400

    use swagger both ways

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -67,6 +67,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{ 
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+}
 
 app.UseHttpsRedirection();
 
